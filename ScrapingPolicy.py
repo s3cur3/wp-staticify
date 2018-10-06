1e310a6bab671b5b64d2c1cc0920a55df9481fbf
Tyler Young <tyler@x-plane.com>
2018-10-05 19:45:00 -0500

    fix (ScrapingPolicy): Fixes to canonicalization and crawling behavior

--- ScrapingPolicy.py
+++ ScrapingPolicy.py
@@ -13,7 +13,7 @@ class ScrapingPolicy:
         :rtype: bool
         :return: true iff this URL should be crawled
         """
-        return self.rootUrl in url
+        return self.rootUrl in url or url.startswith('/')
 
     def shouldScrapeUrl(self, url):
         """
@@ -30,7 +30,15 @@ class ScrapingPolicy:
                  Note that the canonical version should be an absolute URL (e.g., http://mysite.com/home/).
         :rtype: str
         """
-        return url
+        abs_url = self.rootUrl + url if url.startswith('/') else url
+        components = abs_url.split('/')
+        if components[-1]:  # if the path doesn't end in a /
+            if components[-1] == 'index.html':
+                components.pop()  # canonicalize /foo/bar/index.html as /foo/bar/
+            if '.' not in components[-1]:  # last component is "just" a "slug" in WordPress terms, like /foo/bar, not /foo/bar.baz or /foo/bar/
+                components.append('')  # give us a trailing slash!
+            return '/'.join(components)
+        return abs_url
 
     def extractContent(self, content):
         """
--- TestScrapingPolicy.py
+++
@@ -1,8 +0,0 @@
-from ScrapingPolicy import ScrapingPolicy
-
-
-class TestScrapingPolicy(ScrapingPolicy):
-    def __init__(self):
-        super(TestScrapingPolicy, self).__init__("http://conversioninsights.net/")
-
-
